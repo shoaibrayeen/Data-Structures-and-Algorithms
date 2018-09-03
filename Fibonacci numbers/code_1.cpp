c4baee83c13a5c4b996902ed2b94843378b5fcaf
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-09-03 14:15:34 +0530

    Using Recursion

---
+++ Fibonacci numbers/code_1.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 31/07/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include<iostream>
+using namespace std;
+int fib(int num) {
+    if (num <= 1) {
+        return num;
+    }
+    return fib(num-1) + fib(num-2);
+}
+
+int main () {
+    cout << "\nEnter Number\t:\t";
+    int number;
+    cin >> number;
+    cout <<"\nThe result is\t:\t" << fib(number);
+    cout << endl;
+    return 0;
+}
+
