81cea6ec534b0e03e3c6720cb7ec852ca46e3a9d
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-09-16 16:39:24 +0530

    using DP

---
+++ Nth Catalan Number/code_2.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 31/07/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+unsigned long int catalanDP(unsigned int n)  { 
+    catalan[0] = catalan[1] = 1; 
+    for (int i=2; i<=n; i++) { 
+        catalan[i] = 0; 
+        for (int j=0; j<i; j++) 
+            catalan[i] += catalan[j] * catalan[i-j-1]; 
+    } 
+    return catalan[n]; 
+} 
+
+int main () {
+    cout << "\nEnter Number\t:\t";
+    unsigned int number;
+    cin >> number;
+    cout <<"\nThe result is\t:\t" << catalan(number);
+    cout << endl;
+    return 0;
+}
