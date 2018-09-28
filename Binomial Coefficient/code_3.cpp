1dc3e151babdb5bc4272902880a94c4890a08a01
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-09-28 17:44:20 +0530

    using DP - Auxiliary Space: O(k)

---
+++ Binomial Coefficient/code_3.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 31/07/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int binomialCoeff(int n, int k) { 
+    int C[k+1]; 
+    memset(C, 0, sizeof(C)); 
+  
+    C[0] = 1;
+  
+    for (int i = 1; i <= n; i++) {
+        for (int j = min(i, k); j > 0; j--) 
+            C[j] = C[j] + C[j-1]; 
+    } 
+    return C[k]; 
+} 
+ 
+int main () {
+    cout << "\nEnter n\t:\t";
+    int number;
+    cin >> n;
+    cout << "\nEnter k\t:\t";
+    int k;
+    cin >> k;
+    cout <<"\nThe result is\t:\t" << binomialCoeff(n,k);
+    cout << endl;
+    return 0;
+}
