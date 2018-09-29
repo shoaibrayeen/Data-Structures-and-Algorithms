f22b56a08d5ca17e8448ce914cb316c9140ff38e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-09-29 15:55:49 +0530

    using recursion

---
+++ Compute nCr mod p/code_1.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 31/07/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int binomialCoeff(int n, int k) { 
+  if (k==0 || k==n) 
+    return 1; 
+  return  binomialCoeff(n-1, k-1) + binomialCoeff(n-1, k); 
+}
+ 
+ 
+int main () {
+    cout << "\nEnter n\t:\t";
+    int number;
+    cin >> n;
+    cout << "\nEnter k\t:\t";
+    int k;
+    cin >> k;
+    cout << "\nEnter p\t:\t";
+    int p;
+    cin >> p;
+    cout <<"\nThe result is\t:\t" << binomialCoeff(n,k)%p;
+    cout << endl;
+    return 0;
+}
