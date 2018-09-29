9dfa5563b2d85a76cfeb00ff624355debc640805
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-09-29 15:57:32 +0530

    using DP - Auxiliary Space: O(n*k)

---
+++ Compute nCr mod p/code_2.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binomialCoeff(int n, int k , int p ) 
+{ 
+    int C[n+1][k+1]; 
+    int i, j; 
+    for (i = 0; i <= n; i++) { 
+        for (j = 0; j <= min(i, k); j++) { 
+            if (j == 0 || j == i) 
+                C[i][j] = 1; 
+            else
+                C[i][j] = (C[i-1][j-1]%p + C[i-1][j]%p)%p; 
+        } 
+    } 
+  
+    return C[n][k]; 
+}
+ 
+ 
+int main () {
+    cout << "\nEnter n\t:\t";
+    int number;
+    cin >> n;
+    cout << "\nEnter k\t:\t";
+    int k;
+    cin >> k;
+    cout << "\nEnter p\t:\t";
+    int p;
+    cin >> p;
+    cout <<"\nThe result is\t:\t" << binomialCoeff(n,k,p);
+    cout << endl;
+    return 0;
+}
