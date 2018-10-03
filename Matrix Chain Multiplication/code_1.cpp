a78f4f56f841c1a3d20cd36c175b835dd4e6622a
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-03 18:25:28 +0530

    using Recursion

--- Matrix Chain Multiplication/code_1.cpp
+++ Matrix Chain Multiplication/code_1.cpp
@@ -9,31 +9,30 @@
 #include<climits> 
 using namespace std; 
   
-int MatrixChainOrder(int* p, int i, int j) { 
-    if(i == j) {
+int MatrixChainOrder(int* dim, int start, int end) {
+    if(start == end) {
         return 0;
     }
-    int min = INT_MAX; 
-    int count; 
-    for (int k = i; k < j; k++) { 
-        count = MatrixChainOrder(p, i, k) + MatrixChainOrder(p, k+1, j) + p[i-1]*p[k]*p[j]; 
-  
+    int min = INT_MAX;
+    int count;
+    for (int k = start; k < end; k++) {
+        count = MatrixChainOrder(dim, start, k) + MatrixChainOrder(dim, k+1, end) + dim[start-1]*dim[k]*dim[end];
+        
         if (count < min) {
-            min = count; 
+            min = count;
         }
-    } 
-    return min; 
-} 
-  
+    }
+    return min;
+}
 int main()  {
-  int n;
-  cout << "\nEnter Number of Matrix\t:\t";
-  cin >> n;
-  int a[n+1];
-  cout << "\nEnter Dimenstions of Matrices\n";
-  for(int i = 0; i <= n ; i++ ) {
-    cin >> a[i];
-  }
-  cout << "\nMinimum number of multiplications\t:\t" << MatrixChainOrder(arr, 1, n-1));
-  return 0; 
+    int n;
+    cout << "\nEnter Number of Matrix\t:\t";
+    cin >> n;
+    int dim[n+1];
+    cout << "\nEnter Dimenstions of Matrices\n";
+    for(int i = 0; i <= n ; i++ ) {
+        cin >> dim[i];
+    }
+    cout << "\nMinimum number of multiplications\t:\t" << MatrixChainOrder(dim, 1, n) << endl;;
+    return 0;
 }
