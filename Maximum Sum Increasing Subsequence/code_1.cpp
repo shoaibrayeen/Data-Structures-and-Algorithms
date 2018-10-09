cfc93e6272966a45bd21ff8873d2b0aaaad92496
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-09 14:23:58 +0530

    using DP

---
+++ Maximum Sum Increasing Subsequence/code_1.cpp
@@ -0,0 +1,42 @@
+/
+//  main.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 31/07/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+
+int maxSumIS(int *arr, int n) { 
+    int i, j, max = 0; 
+    int msis[n]; 
+    for ( i = 0; i < n; i++ ) {
+        msis[i] = arr[i]; 
+    }
+    for ( i = 1; i < n; i++ ) {
+        for ( j = 0; j < i; j++ ) {
+            if (arr[i] > arr[j] && msis[i] < msis[j] + arr[i]) {
+                msis[i] = msis[j] + arr[i]; 
+            }
+        }
+    }
+    for ( i = 0; i < n; i++ ) {
+        if ( max < msis[i] ) {
+            max = msis[i]; 
+        }
+    }
+    return max; 
+}  
+  
+int main() { 
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    int arr[n];
+    cout << "\nEnter Sequence\n";
+    for(int i =0; i < n; i++ ) {
+      cin >> arr[i];
+    }
+    cout << "\Length of LIS\t:\t" << maxSumIS(arr,n); 
+    return 0; 
