2ea82108be4af48d418181d63c77c6e40738cced
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-11-21 18:54:46 +0530

    Binomial Coefficient using DP

--- Binomial Coefficient/code_2.cpp
+++ Binomial Coefficient/code_2.cpp
@@ -9,8 +9,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binomialCoeff(int n, int k) 
-{ 
+int binomialCoeff(int n, int k) { 
     int C[n+1][k+1]; 
     int i, j; 
     for (i = 0; i <= n; i++) { 
@@ -28,7 +27,7 @@ int binomialCoeff(int n, int k)
  
 int main () {
     cout << "\nEnter n\t:\t";
-    int number;
+    int n;
     cin >> n;
     cout << "\nEnter k\t:\t";
     int k;
