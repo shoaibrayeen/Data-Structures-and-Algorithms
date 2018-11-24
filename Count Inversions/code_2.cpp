7a6ee1795be44e2b4b9e5fb9f6d06341476a3b8d
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-11-25 03:36:10 +0530

    using Merge Sort - Divide and Conquer

---
+++ Count Inversions/code_2.cpp
@@ -0,0 +1,71 @@
+//
+//  code_2.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int _mergeSort(int arr[], int temp[], int left, int right);
+int merge(int arr[], int temp[], int left, int mid, int right);
+
+int mergeSort(int arr[], int array_size) {
+    int* temp = (int*)malloc(sizeof(int) * array_size);
+    return _mergeSort(arr, temp, 0, array_size - 1);
+}
+
+int _mergeSort(int arr[], int temp[], int left, int right) {
+    int mid, inv_count = 0;
+    if (right > left) {
+        mid = (right + left) / 2;
+        inv_count = _mergeSort(arr, temp, left, mid);
+        inv_count += _mergeSort(arr, temp, mid + 1, right);
+        inv_count += merge(arr, temp, left, mid + 1, right);
+    }
+    return inv_count;
+}
+
+int merge(int arr[], int temp[], int left, int mid, int right) {
+    int i, j, k;
+    int inv_count = 0;
+    
+    i = left;
+    j = mid;
+    k = left;
+    while ((i <= mid - 1) && (j <= right)) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        }
+        else {
+            temp[k++] = arr[j++];
+            inv_count = inv_count + (mid - i);
+        }
+    }
+    while (i <= mid - 1) {
+        temp[k++] = arr[i++];
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    for (i = left; i <= right; i++) {
+        arr[i] = temp[i];
+    }
+    return inv_count;
+}
+
+int main(int argv, char** args) {
+    int n;
+    cout << "\nEnter Number of Element\t:\t";
+    cin >> n;
+    cout << "\nEnter Elements\n";
+    int* arr = new int[n];
+    for ( int i = 0; i < n; i++ ) {
+        cin >> arr[i];
+    }
+    cout << "\nNumber of inversions\t:\t" << mergeSort(arr, 5) << endl;
+    return 0;
+}
