d8905d9bd373bce7465d9f067381239531c10ba1
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-12-13 19:42:05 +0530

    using linear Search

---
+++ Find a Fixed Point in a Given Array/code_1.cpp
@@ -0,0 +1,33 @@
+//
+//  code_1.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n) {
+    int i;
+    for(i = 0; i < n; i++) {
+        if(arr[i] == i) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    int arr[n];
+    for( int i = 0; i < n; i++ ) {
+        cin >> arr[i];
+    }
+    cout << "\nFixed Point\t:\t" << linearSearch(arr, n) << endl;
+    return 0;
+}
