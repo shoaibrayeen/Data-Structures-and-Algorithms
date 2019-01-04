f1861444b1f8fa8a23b47031c2de47a5947c9100
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-01-04 22:56:57 +0530

    using Single Traversal

---
+++ DATA STRUCTURES/Array/Move All Zeros to end of Array/code_1.cpp
@@ -0,0 +1,45 @@
+//
+//  code_1.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+void moveZerosToEnd(int arr[], int n) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            swap(arr[count++], arr[i]);
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    int *a = new int[n];
+    cout << "\nEnter First Array Elements\n";
+    for ( int i = 0; i < n; i++ ) {
+        cin >> a[i];
+    }
+    moveZerosToEnd(a, n);
+    cout << "\nArray After Moving Zeros\t:\t";
+    printArray(a, n);
+    return 0;
+}
