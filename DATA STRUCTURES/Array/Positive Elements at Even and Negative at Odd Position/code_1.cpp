9d5a0554df16dfa10895601cfb19e1df72f02eab
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-01-10 10:45:02 +0530

    Go from left to right

---
+++ DATA STRUCTURES/Array/Positive Elements at Even and Negative at Odd Position/code_1.cpp
@@ -0,0 +1,58 @@
+//
+//  code_1.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+/*
+  Index Starting from 0!
+  0 , 2 , 4 , 6 , ... -> Positive Numbers
+  1 , 3 , 5 , 7 , ... -> Negative Numbers
+*/
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+void rearrange(int a[], int n) {
+    cout << "\nArray Before Modification\t:\t";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    int positive = 0, negative = 1;
+    while (true) {
+        while (positive < n && a[positive] >= 0) {
+            positive += 2;
+        }
+        while (negative < n && a[negative] <= 0) {
+            negative += 2;
+        }
+        if (positive < n && negative < n) {
+            swap(a[positive], a[negative]);
+        }
+        else {
+            break;
+        }
+    }
+    cout << "\nArray After Modification\t:\t";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    int *a = new int[n];
+    cout << "\nEnter Array Elements\n";
+    for ( int i = 0; i < n; i++ ) {
+        cin >> a[i];
+    }
+    rearrange( a , n);
+    delete[] a;
+    cout <<  endl;
+    return 0;
+}
