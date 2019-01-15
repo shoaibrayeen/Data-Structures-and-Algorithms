c14e038a8b4bf9ab93ec0f40cb69e2ac68def1a7
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-01-15 19:11:15 +0530

    using XOR

---
+++ DATA STRUCTURES/Array/Find the Missing Number/code_2.cpp
@@ -0,0 +1,41 @@
+//
+//  code_2.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int  getMissingNo (vector<int> a) {
+    unsigned long n = a.size();
+    int first = a[0];
+    for (int i = 1; i< n; i++) {
+        first ^= a[i];
+    }
+    int second = 1;
+    for (int i = 2; i <= n+1; i++) {
+        second ^= i;
+    }
+    return (first^second);
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    vector<int> a(n);
+    cout << "\nEnter Elements\n";
+    for ( int i = 0; i < n; i++ ) {
+        cin >> a[i];
+    }
+    cout << "\nMissing Number\t:\t" << getMissingNo(a);
+    a.clear();
+    cout <<  endl;
+    return 0;
+}
