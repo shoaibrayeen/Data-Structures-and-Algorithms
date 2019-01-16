a9cae3a57ad8fa3dda7af868a4e8cfd8067092cd
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-01-16 16:44:21 +0530

    Dutch National Flag Algorithm

---
+++ DATA STRUCTURES/Array/Sort An Array of 0s - 1s and 2s/code_2.cpp
@@ -0,0 +1,57 @@
+//
+//  code_2.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort(vector <int> a) {
+    long n = a.size();
+    int lo = 0;
+    int hi = n - 1;
+    int mid = 0;
+    while (mid <= hi) {
+        switch (a[mid]) {
+            case 0:
+                swap(&a[lo++], &a[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(&a[mid], &a[hi--]);
+                break;
+        }
+    }
+    cout << "\nAfter Sorting\t:\t";
+    for ( int i  = 0; i < n; i++ ) {
+        cout << a[i] << "  ";
+    }
+    cout << endl;
+}
+int main() {
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    vector<int> a(n);
+    cout << "\nEnter Elements\n";
+    for ( int i = 0; i < n; i++ ) {
+        cin >> a[i];
+    }
+    sort(a);
+    a.clear();
+    cout <<  endl;
+    return 0;
+}
