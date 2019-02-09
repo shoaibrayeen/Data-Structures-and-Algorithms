58466cce6751710b814a782147874e5f52ba621a
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-02-09 21:04:46 +0530

    Naive Approach

---
+++ Minimum Number of Jumps to Reach End/code_1.cpp
@@ -0,0 +1,43 @@
+//
+//  code_1.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int minJumps(int arr[], int l, int h) {
+    if (h == l) {
+        return 0;
+    }
+    if (arr[l] == 0) {
+        return INT_MAX;
+    }
+    int min = INT_MAX;
+    for (int i = l + 1; i <= h && i <= l + arr[l]; i++) {
+        int jumps = minJumps(arr, i, h);
+        if(jumps != INT_MAX && jumps + 1 < min) {
+            min = jumps + 1;
+        }
+    }
+    
+    return min;
+}
+
+
+int main() {
+    int n;
+    cout << "\nNumber Size\t:\t";
+    cin >> n;
+    int array[n];
+    cout << "\nEnter Array Elements\n";
+    for ( int i = 0 ;i < n;i++) {
+        cin >> array[i];
+    }
+    cout << "\nMinimum Jumps\t:\t" << minJumps(array, 0, n-1) << "\n";
+    return 0;
+}
