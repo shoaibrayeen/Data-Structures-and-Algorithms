f83eec347e4d9ea742ad1aa90a23572a584e832a
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-03-01 20:43:56 +0530

    using Modified Binary Search

---
+++ DATA STRUCTURES/Array/Find The Smallest Missing Number/code_1.cpp
@@ -0,0 +1,39 @@
+//
+//  code_1.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getFirstMissing(vector<int> array, int start, int end) {
+    if (start > end) {
+        return end + 1;
+    }
+    if (start != array[start]) {
+        return start;
+    }
+    int mid = (start + end) / 2;
+    if (array[mid] == mid) {
+        return getFirstMissing(array, mid+1, end);
+    }
+    return getFirstMissing(array, start, mid);
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Size of Array\t:\t";
+    cin >> n;
+    vector<int> array(n);
+    cout << "\nEnter Array Element\n";
+    for (int i = 0; i < n; i++ ) {
+        cin >> array[i];
+    }
+    cout << "\nSmallest Missing Number\t:\t" << getFirstMissing(array, 0, n-1) << "\n";
+    return 0;
+}
+
