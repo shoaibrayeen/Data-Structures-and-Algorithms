6dee21206c937d1ce45a9b9cf877a790346f760b
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-03-19 17:03:46 +0530

    using DP - Linear Space

---
+++ Find Maximum Possible Stolen Value From Houses/code_1.cpp
@@ -0,0 +1,43 @@
+//
+//  code_1.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getMaximumStolenValue(vector<int> hval) {
+    int n = int(hval.size());
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return hval[0];
+    }
+    if (n == 2) {
+        return max(hval[0], hval[1]);
+    }
+    int dp[n];    dp[0] = hval[0];
+    dp[1] = max(hval[0], hval[1]);
+    for (int i = 2; i<n; i++) {
+        dp[i] = max(hval[i] + dp[i-2] , dp[i-1]);
+    }
+    return dp[n-1];
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Total Values\t:\t";
+    cin >> n;
+    vector<int> array(n);
+    cout << "\nEnter All Values\n";
+    for ( int i = 0; i < n; i++ ) {
+        cin >> array[i];
+    }
+    cout << "\nMaximum Stolen Value\t:\t" << getMaximumStolenValue(array) << "\n";
+    return 0;
+}
