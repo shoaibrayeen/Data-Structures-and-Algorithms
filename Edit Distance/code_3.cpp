3b41994ede6508e27daa299a4c163c982e1f506f
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-03-25 19:43:39 +0530

    using DP - Linear Space

--- Edit Distance/code_3.cpp
+++ Edit Distance/code_3.cpp
@@ -5,31 +5,38 @@
 //  Created by Mohd Shoaib Rayeen on 17/10/18.
 //  Copyright © 2018 Shoaib Rayeen. All rights reserved.
 //
+
+
 #include<iostream>
 #include<string.h>
 using namespace std;
 
+int min(int x, int y, int z) {
+    return min(min(x, y), z);
+}
+
 int editDist(string str1 , string str2) {
     int m  = int(str1.length());
     int n  = int(str2.length());
-    int table[2][n+1];
-    for(int i = 0 ; i <= m; i++ ) {
-        for(int j = 0; j <= n; j++ ) {
-            if ( i == 0 ) {
-                table[i%2][j] = j;
+    int dp[2][n+1];
+    for (int i = 0; i <= m; i++ ) {
+        for (int j = 0; j <= n; j++ ) {
+            if (i == 0) {
+                dp[i % 2][j] = j;
             }
-            if ( j == 0 ) {
-                table[i%2][j] = i;
+            else if (j == 0) {
+                dp[i % 2][j] = i;
             }
-            if ( str1[i-1] == str2[i-1] ) {
-                table[i%2][j] = table[(i-1)%2][j-1];
+            else if (str1[i-1] == str2[j-1]) {
+                dp[i % 2][j] = dp[(i-1) % 2][j-1];
             }
             else {
-                table[i%2][j] = 1 + min( min(table[i%2][j-1] , table[(i-1)%2][j] ) , table[(i-1)%2][j-1]);
+                dp[i % 2][j] = 1 + min(dp[i%2][j-1], dp[(i-1) % 2][j], dp[(i-1) % 2][j-1]);
             }
         }
     }
-    return table[m%2][n];
+    
+    return dp[m % 2][n];
 }
 
 int main() {
