aa9729003f3985b041f5a499c3827e3608db84e6
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-04-12 18:22:19 +0530

    using Recursion

---
+++ DATA STRUCTURES/Binary Tree/Sum of All Nodes/code_1.cpp
@@ -0,0 +1,82 @@
+//
+//  code_1.cpp
+//  Data Structure
+//
+//  Created by Mohd Shoaib Rayeen on 19/03/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+struct bnode {
+    int data;
+    bnode *left;
+    bnode *right;
+};
+
+class btree {
+public:
+    bnode *root , *root2;
+    btree();
+    void insert();
+    void display();
+    bnode* newNode(int);
+    int sumNodes(bnode*);
+};
+btree::btree() {
+    
+    root = NULL;
+}
+bnode* btree:: newNode(int value) {
+   
+    bnode* temp=new bnode;
+    temp->data=value;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+
+int btree::sumNodes(bnode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return (root->data + sumNodes(root->left) + sumNodes(root->right));
+        
+}
+
+void btree:: insert() {
+    root = newNode(8);
+    root->left = newNode(3);
+    root->right = newNode(10);
+    root->left->left = newNode(1);
+    root->left->right = newNode(6);
+    root->right->right = newNode(14);
+    root->right->right->left = newNode(13);
+    root->left->right->left = newNode(4);
+    root->left->right->right = newNode(7);
+    /*
+    root = newNode(10);
+    root->left = newNode(12);
+    root->right = newNode(3);
+    root->left->right = newNode(4);
+    root->right->left = newNode(5);
+    root->right->left->right = newNode(6);
+    root->right->left->right->left = newNode(18);
+    root->right->left->right->right = newNode(7);
+  */
+}
+void btree::display() {
+    insert();
+    cout << "\nSum of All Nodes\t:\t" << sumNodes(root);
+}
+
+
+
+
+
+int main() {
+    btree obj;
+    obj.display();
+    cout << "\n";
+    return 0;
+}
+
