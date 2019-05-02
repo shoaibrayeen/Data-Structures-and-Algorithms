734f3a40449e1cac773c578362cd19a2bc5f0d97
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-05-02 16:43:28 +0530

    using Recursion

---
+++ DATA STRUCTURES/Binary Tree/Print All Full Nodes/code_1.cpp
@@ -0,0 +1,75 @@
+//
+//  code_1.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 15/04/19.
+//  Copyright © 2019 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+struct bnode {
+    int data;
+    bnode *left;
+    bnode *right;
+};
+
+class btree {
+public:
+    bnode *root;
+    btree();
+    void insert();
+    void display();
+    bnode* newNode(int);
+    void findFullNode(bnode*);
+};
+btree::btree() {
+    
+    root = NULL;
+}
+bnode* btree:: newNode(int value) {
+    
+    bnode* temp=new bnode;
+    temp->data=value;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+void btree:: insert() {
+    root              = newNode(1);
+    root->left        = newNode(2);
+    root->right       = newNode(8);
+    root->left->left  = newNode(3);
+    root->left->right = newNode(4);
+    root->right->left  = newNode(9);
+    root->right->right = newNode(10);
+}
+
+
+void btree::display() {
+    insert();
+    cout << "\nFull Nodes\t:\t";
+    findFullNode(root);
+    cout << endl;
+}
+
+
+void btree::findFullNode(bnode *root) {
+    if (root != NULL) {
+        findFullNode(root->left);
+        if (root->left != NULL && root->right != NULL) {
+            cout << root->data << "\t";
+        }
+        findFullNode(root->right);
+    }
+}
+
+int main() {
+    btree obj;
+    obj.display();
+    cout << "\n";
+    return 0;
+}
