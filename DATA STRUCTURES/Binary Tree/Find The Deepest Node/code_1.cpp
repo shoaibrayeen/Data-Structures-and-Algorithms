0bbf57ea090d300e5606eaefb86755f6013ec64b
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-05-30 21:48:48 +0530

    Using Level of Node - Recursion

---
+++ DATA STRUCTURES/Binary Tree/Find The Deepest Node/code_1.cpp
@@ -0,0 +1,81 @@
+//
+//  code_1.cpp
+//  Data Structure
+//
+//  Created by Mohd Shoaib Rayeen on 19/03/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+#include <queue>
+using namespace std;
+ 
+struct bnode {
+    int data;
+    bnode *left;
+    bnode *right;
+};
+
+class btree {
+public:
+    bnode *root;
+    btree();
+    void insert();
+    void display();
+    bnode* newNode(int);
+    void find(bnode* root , int level, int &maxLevel, int &res);
+    int deepestNode(bnode *root);
+};
+btree::btree() {
+    root = NULL;
+}
+bnode* btree:: newNode(int value) {
+    bnode* temp=new bnode;
+    temp->data=value;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+
+void btree:: insert() {
+    root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->right->left = newNode(5);
+    root->right->right = newNode(6);
+    root->right->left->right = newNode(7);
+    root->right->right->right = newNode(8);
+    root->right->left->right->left = newNode(17);
+}
+
+void btree::display() {
+    insert();
+    cout << "\nDeepest Node\t:\t" << deepestNode(root);
+}
+
+int btree::deepestNode(bnode *root) {
+    int res = -1;
+    int maxLevel = -1;
+    find(root, 0, maxLevel, res);
+    return res;
+}
+
+void btree::find(bnode* root , int level, int &maxLevel, int &res) {
+    if (root != NULL) {
+        find(root->left, ++level, maxLevel, res);
+        if (level > maxLevel) {
+            res = root->data;
+            maxLevel = level;
+        }
+        find(root->right, level, maxLevel, res);
+    }
+}
+
+int main() {
+    btree obj;
+    obj.display();
+    cout << "\n";
+    return 0;
+}
