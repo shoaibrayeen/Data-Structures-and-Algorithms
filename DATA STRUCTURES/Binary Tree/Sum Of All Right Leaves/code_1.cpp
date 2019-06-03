e4a0080d6a746e36383f0d4954028ec6daf26026
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-06-03 17:01:31 +0530

    using Recursion with checking each node as Leaf

---
+++ DATA STRUCTURES/Binary Tree/Sum Of All Right Leaves/code_1.cpp
@@ -0,0 +1,87 @@
+//
+//  code_1.cpp
+//  Data Structure
+//
+//  Created by Mohd Shoaib Rayeen on 19/03/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+using namespace std;
+ 
+struct bnode {
+    int data;
+    bnode *left;
+    bnode *right;
+};
+
+class btree {
+public:
+    bnode *root;
+    btree();
+    void insert();
+    void display();
+    bnode* newNode(int);
+    bool isLeaf(bnode*);
+    int rightLeavesSum(bnode*);
+};
+btree::btree() {
+    root = NULL;
+}
+bnode* btree:: newNode(int value) {
+    bnode* temp=new bnode;
+    temp->data=value;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+
+void btree:: insert() {
+    root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->right->left = newNode(5);
+    root->right->right = newNode(6);
+    root->right->left->right = newNode(7);
+    root->right->right->right = newNode(8);
+    root->right->left->right->left = newNode(17);
+}
+
+void btree::display() {
+    insert();
+    cout << "\nSum of Right Leaves\t:\t" << rightLeavesSum(root);
+}
+
+bool btree::isLeaf(bnode *node) {
+    if (node == NULL) {
+        return false;
+    }
+    if (node->left == NULL && node->right == NULL) {
+        return true;
+    }
+    return false;
+}
+
+int btree::rightLeavesSum(bnode *root) {
+    int res = 0;
+    if (root != NULL) {
+        if (isLeaf(root->right)) {
+            res += root->right->data;
+        }
+        else {
+            res += rightLeavesSum(root->right);
+        }
+        res += rightLeavesSum(root->left);
+    }
+    return res;
+}
+
+
+int main() {
+    btree obj;
+    obj.display();
+    cout << "\n";
+    return 0;
+}
