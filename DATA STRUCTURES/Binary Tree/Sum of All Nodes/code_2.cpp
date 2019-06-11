c60e43966d80f7009df23b080be8b59ada5a9684
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-06-11 20:56:09 +0530

    Iterative Approach - using Stack

---
+++ DATA STRUCTURES/Binary Tree/Sum of All Nodes/code_2.cpp
@@ -0,0 +1,84 @@
+//
+//  code_1.cpp
+//  Data Structure
+//
+//  Created by Mohd Shoaib Rayeen on 19/03/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include<iostream>
+#include <stack>
+using namespace std;
+
+
+struct bnode {
+    int data;
+    bnode *left;
+    bnode *right;
+};
+
+class btree {
+public:
+    bnode *root;
+    btree();
+    void insert();
+    void display();
+    bnode* newNode(int);
+    int sumNodes(bnode*);
+};
+btree::btree() {
+    
+    root = NULL;
+}
+bnode* btree:: newNode(int value) {
+    
+    bnode* temp=new bnode;
+    temp->data=value;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+
+int btree::sumNodes(bnode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    stack<bnode*> s;
+    s.push(root);
+    int sum = 0;
+    while(!s.empty()) {
+        root = s.top();
+        s.pop();
+        sum += root->data;
+        if ( root->left) {
+            s.push(root->left);
+        }
+        if ( root->right) {
+            s.push(root->right);
+        }
+    }
+    return sum;
+}
+
+void btree:: insert() {
+    root = newNode(8);
+    root->left = newNode(3);
+    root->right = newNode(10);
+    root->left->left = newNode(1);
+    root->left->right = newNode(6);
+    root->right->right = newNode(14);
+    root->right->right->left = newNode(13);
+    root->left->right->left = newNode(4);
+    root->left->right->right = newNode(7);
+}
+void btree::display() {
+    insert();
+    cout << "\nSum of All Nodes\t:\t" << sumNodes(root);
+}
+
+int main() {
+    btree obj;
+    obj.display();
+    cout << "\n";
+    return 0;
+}
