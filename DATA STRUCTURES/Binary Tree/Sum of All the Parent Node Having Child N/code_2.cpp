34a632a0a1d61f8dcb7a4ce47f497ebe9a39aa38
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-06-25 16:06:48 +0530

    using Static Variable

---
+++ DATA STRUCTURES/Binary Tree/Sum of All the Parent Node Having Child N/code_2.cpp
@@ -0,0 +1,77 @@
+//
+//  code_2.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 15/04/19.
+//  Copyright © 2019 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+using namespace std;
+struct bnode {
+    int data;
+    bnode *left;
+    bnode *right;
+};
+
+class btree {
+public:
+    bnode *root;
+    btree();
+    void insert();
+    void display();
+    bnode* newNode(int);
+    int sumOfParentOfX(bnode*  , int);
+};
+btree::btree() {
+    
+    root = NULL;
+}
+bnode* btree:: newNode(int value) {
+    
+    bnode* temp=new bnode;
+    temp->data=value;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+void btree:: insert() {
+    root = newNode(4);
+    root->left = newNode(2);
+    root->right = newNode(5);
+    root->left->left = newNode(7);
+    root->left->right = newNode(2);
+    root->right->left = newNode(2);
+    root->right->right = newNode(3);
+}
+
+
+void btree::display() {
+    
+    insert();
+    int n;
+    cout << "\nEnter Node\t:\t";
+    cin >> n;
+    cout << "\nSum of Nodes\t:\t" << sumOfParentOfX(root, n);
+}
+
+int btree::sumOfParentOfX(bnode* root, int x) {
+    static int sum = 0;
+    if (!root) {
+        return 0;
+    }
+    if ((root->left && root->left->data == x) || (root->right && root->right->data == x)) {
+        sum += root->data;
+    }
+    sumOfParentOfX(root->left, x);
+    sumOfParentOfX(root->right, x);
+    return sum;
+}
+
+int main() {
+    btree obj;
+    obj.display();
+    cout << "\n";
+    return 0;
+}
