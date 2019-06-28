ebb4d17f6fea0d662aed6ed00ec60bbe04e0a476
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-06-28 19:47:32 +0530

    using Set

---
+++ DATA STRUCTURES/Array/Kth Smallest Element/code_6.cpp
@@ -0,0 +1,40 @@
+//
+//  code_6.cpp
+//  Algorithm
+//
+//  Created by Mohd Shoaib Rayeen on 23/11/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int kthSmallest(int arr[], int n, int k) {
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        s.insert(arr[i]);
+    }
+    auto it = s.begin();
+    for (int i = 0; i < k - 1; i++)
+        it++;
+    return *it;
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    int *a = new int[n];
+    cout << "\nEnter Array Elements\n";
+    for ( int i = 0; i < n; i++ ) {
+        cin >> a[i];
+    }
+    int k;
+    cout << "Enter K\t:\t";
+    cin >> k;
+    cout << "\nKth Smallest Element\t:\t" << kthSmallest( a , n , k ) << endl;
+    delete[] a;
+    return 0;
+}
