0d61cd02c31fe8f022d70aeca4f986375d9895c1
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-06-29 20:03:50 +0530

    using Set to maintain Order

--- DATA STRUCTURES/Array/Kth Smallest Element/code_6.cpp
+++ DATA STRUCTURES/Array/Kth Smallest Element/code_6.cpp
@@ -17,8 +17,9 @@ int kthSmallest(int arr[], int n, int k) {
         s.insert(arr[i]);
     }
     auto it = s.begin();
-    for (int i = 0; i < k - 1; i++)
+    for (int i = 0; i < k - 1; i++) {
         it++;
+    }
     return *it;
 }
 
